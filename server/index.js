abf6826c28dff832e09565dd5a75834171836578
Caique <asapcaique7@gmail.com>
2023-12-29 17:59:50 -0300

    conecção ao banco de dados

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.listen(3002, () => [
-    console.log("rodando")
-]);
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "gloryboy163",
+    database: "crudgames",
+});
+
+app.get('/', (req, res) => {
+    let sql = "INSERT INTO games (name, cost, category) VALUES ('Far Cry 5', '120', 'Ação')";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Registro inserido com sucesso!");
+        }
+    });
+});
+
+app.listen(3003, () => {
+    console.log("O servidor está rodando na porta 3003");
+});
