824a8f6e5841f86a90d591e82513d4ab8bf66092
Caique <asapcaique7@gmail.com>
2023-12-29 17:09:37 -0300

    montando servidor

--- client/crud/src/App.jsx
+++ client/crud/src/App.jsx
@@ -1,9 +1,31 @@
-import './App.css'
+import React, { useState}  from 'react';
+import './App.css';
+
 
 function App() {
-  return(
-    <div>
 
+  const [values, setValues] = useState();
+
+  const handleChangeValues = (value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
+  }; 
+
+  return(
+    <div className='app-container'>
+      <div className='register-container'>
+        <h1 className='reg-title'>Wav Shop</h1>
+        <input type="text" name='name' placeholder='Nome'  className='reg-input' onChange={handleChangeValues}/>
+        <input type="text" name='cost' placeholder='Preço'  className='reg-input' onChange={handleChangeValues}/>
+        <input type="text" name='category' placeholder='Categoria'  className='reg-input' onChange={handleChangeValues}/>
+        <button className='reg-button' onClick={handleClickButton}>Cadastro</button>
+      </div>
     </div>
   )
 }
